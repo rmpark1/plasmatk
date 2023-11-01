8f1ade80a79226103b44045575066e3cc5d09da4
Ryan Park <ryanpark1200@gmail.com>
2023-11-01 14:26:29 -0400

    docs: reconfigure sphinx structure to source build

--- docs/conf.py
+++ docs/conf.py
@@ -16,10 +16,14 @@ author = u"Ryan Park"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "myst_nb",
     "autoapi.extension",
     "sphinx.ext.napoleon",
+    "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.duration",
 ]
 autoapi_dirs = ["../src"]
 
@@ -28,9 +32,21 @@ autoapi_dirs = ["../src"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
+intersphinx_mapping = {
+    "python": ("http://docs.python.org/", None),
+    "pandas": ("http://pandas.pydata.org/pandas-docs/dev", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "sklearn": ("https://scikit-learn.org/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+}
+
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
