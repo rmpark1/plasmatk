298918dd3dae174ba65b272656270e9575ef6f47
Ryan Park <ryanpark1200@gmail.com>
2023-11-03 15:52:16 -0400

    test: add plotting test for mag regime

--- tests/test_parameters.py
+++ tests/test_parameters.py
@@ -1,7 +1,8 @@
 import plasmatk as ptk
 
 def test_Beta_e():
+    """Ensure magnetization parameter is 
+    of the correct order of magnitude."""
     BE = ptk.parameters.Beta_e
     assert BE(200, 1e-15) > 10
     assert BE(200, 1e-15) >= 100
-
---
+++ tests/test_plotting.py
@@ -0,0 +1,8 @@
+import plasmatk as ptk
+from plasmatk import plotting as ppl
+
+def test_plot_mag_regimes():
+    fig, ax = ppl.plot_mag_regimes()
+
+def test_plot_mag_regimes_nT():
+    pass
