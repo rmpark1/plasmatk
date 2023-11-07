d78235ec303dbc93859f7ef804848b9c5de352b6
Ryan Park <ryanpark1200@gmail.com>
2023-11-07 14:08:23 -0500

    test: update mag-coup plot test with label checks

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,8 +1,19 @@
+import matplotlib as mpl
+
 import plasmatk as ptk
 from plasmatk import plotting as ppl
 
 def test_plot_mag_regimes():
-    fig, ax = ppl.plot_mag_regimes()
+    # Test with text
+    fig, ax = ppl.plot_mag_regimes(region_txt=True)
+    assert len([a for a in ax.get_children()
+               if isinstance(a, mpl.text.Text)
+               and "$" in a.get_text()]) == 4
+    # Test without text
+    fig, ax = ppl.plot_mag_regimes(region_txt=False)
+    assert len([a for a in ax.get_children()
+               if isinstance(a, mpl.text.Text)
+               and "$" in a.get_text()]) == 0
 
 def test_plot_mag_regimes_nT():
     pass
